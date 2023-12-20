2487054e9c04c9923989af43fb3ed17186570bff
bipin13071989 <bipin13071989@gmail.com>
2023-12-21 02:05:50 +0530

    new folder is been created just for fun

---
+++ checking/check.js
@@ -0,0 +1,28 @@
+// function greeting(msg) {
+//     console.log('This is your message :' +msg);
+// }
+// greeting('Hello world!');
+
+// function checkingRand(first,second) {
+//     let firstNum=Math.random()*second +first;
+//     let secondNum=Math.random()*second;
+//     let ans=firstNum>4?true:false;
+//     console.log(`First number is: ${firstNum} and second number is: ${secondNum} and ans is ${ans}`);
+// }
+// checkingRand(2,3);
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// arr.push(19);
+// // console.log(arr);
+// arr.unshift(0);
+// console.log(typeof(arr));
+
+// console.log(arr);
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+let singleQuote='Hii my name is bipin kumar jha.';
+console.log(singleQuote);
+let arrStr=singleQuote.split(' ');
+// console.log(singleQuote.join('$'));
+let str=arrStr.join('+');
+console.log(str);

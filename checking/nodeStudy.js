e47c148a7f62665c58c49fa6c8af77b16ae64cdb
bipin13071989 <bipin13071989@gmail.com>
2023-12-21 14:29:15 +0530

    New Topic learned like path

--- checking/nodeStudy.js
+++ checking/nodeStudy.js
@@ -14,6 +14,39 @@
 
 
 let fs=require('fs');
-let buffer = fs.readFileSync('./checking/check.js');
-console.log(buffer.toString('UTF-8')); // That's how we deal with binary buffer
+// let buffer = fs.readFileSync('./checking/check.js');
+// console.log(buffer.toString('UTF-8')); // That's how we deal with binary buffer
+
+// Now we are going to create, read, update, delete
+// fs.openSync('./checking/abc.txt','w');
+// fs.writeFileSync('./checking/abc.txt','we are very happy today and wish you are also good with the life');
+
+// fs.appendFileSync('abc.txt','Hello world!') // add content to already exisiting file 
+
+// // fs.mkdirSync('./checking/NewFolderNamkaran');
+// let content=fs.readdirSync('checking');
+// // console.log(content);
+
+// for(let i=0;i<content.length;i++) {
+//     console.log(content[i]);
+//     //fs.unlinkSync('folder_name'+content[i]);
+// }
+
+
+//It is better to check whether file exist or not using//
+// fs.existsSync()
+
+// for(let i=1;i<=10;i++) {
+//     let fileName=`./checking/NewFolderNamkaran/Lecture-${i}`;
+//     fs.mkdirSync(fileName);
+//     fs.writeFileSync(fileName + "\\"+"readme.md",`Hello world!`);
+// }
+
+// let path =require('path');
+// // console.log(__dirname);
+// let path_name=path.extname(path.join(__dirname,'./checking/check.js'));
+// console.log(path_name);
+// let bsName=path.basename(path.join(__dirname,'./checking/check.js'));
+// console.log(bsName);
+
 
